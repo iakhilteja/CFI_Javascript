b7f7cfc54261398014371a2ef9588ce118ee3589
iakhilteja <akhiltejabm@gmail.com>
2022-07-20 10:16:57 +0530

    Bitwise, Unary, type casting

---
+++ Operators/7.js
@@ -0,0 +1,59 @@
+// Bitwise Operators
+// Bitwise AND - &
+// Bitwise OR - |
+// Bitwise NOT - ~
+// Bitwise XOR - ^
+
+// Bitwise AND - &
+// a  b  r(a&b)
+// 0  0  0
+// 0  1  0
+// 1  0  0
+// 1  1  1
+
+// var a=40,b=25
+// 7 = 0111
+// 5 = 0101
+// --------
+//     0101    =  5
+// console.log(a&b)
+// console.log(a&b)
+
+
+var a=53,b=26
+// Bitwise OR - |
+// a  b  r(a|b)
+// 0  0  0
+// 0  1  1
+// 1  0  1
+// 1  1  1
+
+// console.log(a|b)
+// console.log(a|b)
+
+// var a=73,b=45
+// Bitwise XOR - ^
+// a  b  r(a^b)
+// 0  0  0
+// 0  1  1
+// 1  0  1
+// 1  1  0
+
+// console.log(a^b)
+
+var a=17,b=4
+// Left Shift (<<)
+// console.log(a<<b)
+
+
+// Right shift (>>)
+
+// var a=117,b=3
+// console.log(a>>b)
+
+// Bitwise NOT - ~
+// ~N = -(N+1)
+console.log(~2)
+
+
+
---
+++ Operators/8.js
@@ -0,0 +1,102 @@
+// Binary - 0bnnnn (2)
+// Decimal - nnn (10)
+// Octal - 0onnn (8)
+// Hexadecimal - 0xnnn (16)
+
+// toString
+// var a = 1000
+
+// console.log(a.toString(10))  //To convert to decimal
+// console.log(a.toString(2))  // To convert to BINARY
+// console.log(a.toString(8))  // To convert to OCTAL
+// console.log(a.toString(16))  // To convert to HEXA-DECIMAL
+
+
+// Unary Operator
+
+// +,-
+
+// console.log(1)
+// console.log(+1)
+// console.log(-1)
+
+
+// console.log('1')
+// console.log(+'1')
+// console.log(-'1')
+
+// console.log(+'t')
+// console.log(-'t')
+
+// console.log(typeof NaN)
+
+// var a = 1,b='1',c=true,d = 'true'
+// console.log(a, typeof a)
+// console.log(b, typeof b)
+// console.log(c, typeof c)
+// console.log(d, typeof d)
+
+// console.log(+a, typeof +a)
+// console.log(+b, typeof +b)
+// console.log(+c, typeof +c)
+// console.log(+d, typeof +d)
+
+// console.log(-a, typeof -a)
+// console.log(-b, typeof -b)
+// console.log(-c, typeof -c)
+// console.log(-d, typeof -d)
+
+
+// Type casting: Converting one datatype to other
+// When the operand of an operator are different data types, one of them will be
+// converted to an 'equivalent' value of the other operands type.
+// */
+
+// console.log(1+1+1+1)
+
+// console.log(1+'1')
+
+// console.log('1'+1+1+1)
+
+// console.log(1+'1'+1+1)
+
+// console.log(1+1+'1'+1)
+
+// console.log(1+1+1+'1')
+
+// console.log(false + false)
+// console.log(false + true)
+// console.log(false + 0)
+// console.log(true+ 0)
+// console.log(1+ true)
+
+
+// console.log('hi Code for India foundation')
+// console.log('hi','Code for India', 'foundation')
+// console.log('hi'+ 'Code for India' +'foundation')
+
+// console.log('hi','Code for India', 'foundation',2)
+// console.log('hi'+ 'Code for India' +'foundation' + 2)
+
+// console.log(12+'hi')
+// console.log('12' + 'hi')
+// console.log('hi'+12)
+
+// console.log('100' + 1 + 2 + '3' + 4) 
+// console.log(100 + 1 + 2 + '3' + 4) 
+
+// console.log('1' + '2') //12
+// console.log('1' + 2) //12
+// console.log(1 + '2')  // 12
+// console.log(100 + 1 + '2')  // 1012
+// console.log('100' + 1 + '2')  // 10012
+// console.log('100' + 1 + 2)  // 10012
+
+
+
+console.log('1' - '2') //
+console.log('1' - 2) // 
+console.log(1 - '2')  // 
+console.log(100 - 1 + '2')  // 
+console.log('100' - 1 - '2')  // 
+console.log('100' - 1 - 2)  // 
