7caa25a1fa88c4f6c01dff4e2484fde584bccea2
iakhilteja <akhiltejabm@gmail.com>
2022-07-27 11:06:17 +0530

    Iterations over datatypes

---
+++ Iterations/18.js
@@ -0,0 +1,77 @@
+// Write a program to print multiples of 7 between 1-200?
+// i=1
+// while (i<=200){
+//     if ((i%7)==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+// Write a program to print 2 table using while loop?
+
+// i=1
+// while (i<=10){
+//     console.log(` 2 x ${i} = ${i*2}`)
+//     i++
+// }
+
+
+// Write a program to print individual character of the string using while loop?
+
+a = 'Code'
+// console.log(a.length)
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+
+// i=0
+// while (i<(a.length)){
+//     console.log(a[i])
+//     i++
+// }
+
+// Even Characters only
+// a = 'CodeForIndia'
+// i=1
+// while (i<(a.length)){
+//     console.log(a[i])
+//     i+=2
+// }
+
+
+// a = ['a','b','c','d']
+// i=0
+// while (i<(a.length)){
+//     console.log(a[i])
+//     i++
+// }
+
+
+a={1:'a',2:'b',3:'c',4:'d'}
+console.log(a)
+// console.log(Object.keys(a))
+// console.log(Object.values(a))
+
+l = Object.keys(a)
+v = Object.values(a)
+console.log(l.length)
+
+i=0
+while (i<l.length){
+    console.log(l[i],':',v[i])
+    i++
+}
+
+
+// Confirm whether given number is a 3 digit number?
+
+// Confirm whether given number is an 'N' digit number?
+
+//  student > 75%
+// if he has more than 75, allow to exam
+// if he doesnt has ask for permission, if yes allow him, if not dont allow him
+
+// Find whether a given number is a prime number or not?
+
