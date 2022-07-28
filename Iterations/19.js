7649edc89838a35bb18986ba1ebf5c4d880bb2ef
iakhilteja <akhiltejabm@gmail.com>
2022-07-28 09:39:45 +0530

    for loop iterations

---
+++ Iterations/19.js
@@ -0,0 +1,76 @@
+// Print 1 to 5 using for loop?
+// for (i=1;i<=5;i++){
+//     console.log(i)
+// }
+
+// Iteration-1: i=1; i<=5; 1
+// Iteration-2: i=2; i<=5; 2
+// Iteration-3: i=3; i<=5; 3
+// Iteration-4: i=4; i<=5; 4
+// Iteration-5: i=5; i<=5; 5
+// Iteration-6: i=6; false;stop
+
+// Print 10 to 5 using for loop?
+// for (i=10;i>=5;i--){
+//     console.log(i)
+// }
+
+// Write a program to print even numbers between 50-100?
+// for (i=50;i<=100;i=i+2){
+//     console.log(i)
+// }
+
+// for (i=50;i<=100;i++){
+//     if ((i%2)==0){
+//         console.log(i)
+//        } 
+// }
+
+// WAP to print odd numbers between 1-100
+// for (i=1;i<=100;i++){
+//     if ((i%2)!=0){
+//         console.log(i)
+//        } 
+// }
+// WAP to print 10 table?
+// for (i=1;i<=10;i++){
+//     console.log(`10 x ${i} = ${10 * i}`)
+// }
+
+// Nested Loops
+// for (i=0;i<=2;i++){
+//     for (j=0;j<=2;j++){
+//         console.log(i,j)
+//     }
+// }
+
+
+// Print Multiple table
+// var start=10,end=20
+// for (i=start; i<=end;i++){
+//     console.log(`-------- ${i} Table ------`)
+//     for (j=1;j<=10;j++){
+//         console.log(`${i} x ${j} = ${i*j}`)
+//     }
+// }
+
+var start=10,end=20
+
+while (start<=end){
+    console.log(`-------- ${start} Table ------`)
+    i=1
+    while (i<=10){
+        console.log(`${start} x ${i} = ${start*i}`)
+        i++
+    }
+    start++;
+}
+
+
+
+
+
+
+
+
+
