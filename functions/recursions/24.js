ce99fa9b896d6747c34069dbcd23e2fd58cc8a6e
iakhilteja <akhiltejabm@gmail.com>
2022-08-02 20:01:41 +0530

    recursions

---
+++ functions/recursions/24.js
@@ -0,0 +1,68 @@
+// Recursions
+
+i=1
+function f1(){
+    if (i==10){
+        return
+    }
+    else{
+        console.log(i)
+        return f1(i++)
+    }
+}
+
+f1()
+
+
+// z=5
+// function f1(z){
+//     if (i==10){
+//         return
+//     }
+//     else{
+//         console.log(i)
+//         return f1(i++)
+//     }
+// }
+(i){
+    //     if (i==6){
+    //         return
+    //     }
+    //     else{
+    //         console.log(i)
+    //         return mul(i++)
+    //     }
+    // }
+    // mul(1)mul(i){
+//     if (i==151){
+//         return
+//     }
+//     else{
+//         if ((i%3)==0){
+//             console.log(i)
+//         }
+//         return mul(i++)
+//     }
+// }
+// mul(50(i){
+//     if (i==6){
+//         return
+//     }
+//     else{
+//         console.log(i)
+//         return mul(i++)
+//     }
+// }
+// mul(1))
+
+
+// function mul(i){
+//     if (i==6){
+//         return
+//     }
+//     else{
+//         console.log(i)
+//         return mul(i++)
+//     }
+// }
+// mul(1)
