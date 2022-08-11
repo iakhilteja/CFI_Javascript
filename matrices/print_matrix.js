58c462c1fe34faaeefbade760d8c549649cbcee9
iakhilteja <akhiltejabm@gmail.com>
2022-08-11 09:16:49 +0530

    Matrix input done

--- matrices/print_matrix.js
+++ matrices/print_matrix.js
@@ -1,4 +1,25 @@
 import readlineSync from "readline-sync"
 
-let a = readlineSync.question("Please give me value of a: ")
-console.log(a)
+function MatrixInput(){
+    let rows = readlineSync.questionInt("Enter number of rows: ")
+    let cols = readlineSync.questionInt("Enter number of columns: ")
+    console.log(`Given matrix is ${rows} x ${cols} dimension`)
+    
+    let MatrixA = new Array(rows) //[_ _ _]
+    // console.log(MatrixA.length)
+    for (let i=0;i<MatrixA.length;i++){
+        MatrixA[i] = new Array(cols)
+    } //[[][][]]
+    // console.log(MatrixA)
+
+    for (let i=0;i<rows;i++){
+        for (let j=0;j<cols;j++){
+            MatrixA[i][j] = readlineSync.questionInt(`Enter element of ${i} ,${j}: `)
+        }
+    }
+    return MatrixA
+
+}
+console.log(MatrixInput())
+// MatrixInput()
+
